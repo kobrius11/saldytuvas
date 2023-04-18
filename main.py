5f84aa0082dced176309ab1884f3f1c717d2890d
Zygimantas B <kobrius11@gmail.com>
2023-04-18 15:02:22 +0300

    Revert "update with total mass (#6)" (#7)
    This reverts commit 47d0608d921e65f3c152a90167073b0dd5916899.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+saldytuvas = {"morkos" : 2, "burokai" : 2, "bananai" : 5, "bulves:" : 7, "pienas" : 1, "apelsinai" : 2}
 
-saldytuvas = {"morkos": 3, "burokai": 2, "bananai": 5, "bulves": 7, "pienas": 1, "apelsinai": 2}
 
 
 def remove_product():
-    name = input('Įveskite produkto pavadinimą: ')
+    name = input('iveskite produkta: ')
     if name in saldytuvas.keys():
         ###
         if saldytuvas[name].get("quantity") > 1:
@@ -12,15 +12,15 @@ def remove_product():
             saldytuvas[name] = istrinti - 1
         else:
             saldytuvas.pop(name)
-        return
+        return 
 
+def add_product(product_name, quantity=1):
+    ###
 
-def add_product(product_name, unit):
+    product_type = bool(input("ar produktas skystas ?: "))
     quantity = float(input("Įveskite kiekį: "))
-    if unit == "kg":
-        quantity *= 1  # convert kg to g
-    if product_name in saldytuvas:
-        saldytuvas[product_name] += quantity
+    if product_name in saldytuvas: 
+        saldytuvas[product_name] += {"quantity": quantity, "Type": product_type}
     else:
         saldytuvas[product_name] = {"quantity": quantity, "Type": product_type}
     print("Produktas sėkmingai pridėtas į šaldytuvą!\n")
@@ -28,38 +28,33 @@ def add_product(product_name, unit):
 
 def view_product():
     for key, value in saldytuvas.items():
-        print(key, value)
+        print( key, value)
 
-def total_mass():
-    total_kg = 0
-    total_l = 0
-    for key, value in saldytuvas.items():
-        if key in ["morkos", "burokai", "bulves"]:
-            total_kg += value
-        elif key in ["pienas"]:
-            total_l += value
-        elif key in ["bananai", "apelsinai"]:
-            total_kg += value * 0.5  # assuming an average weight of 0.5 kg per fruit
-    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l".format(total_kg/1000, total_l))
 
 while True:
     print("""
-    1- pridėti produktą
-    2- išimti produktą
-    3- apžiūrėti produktus šaldytuve
-    4- peržiūrėti bendrą produktų masę
+    1- prideti produkta
+    2- isimti produkta
+    3- apziureti produktus saldytuve
 
-    0- išeiti
-
+    0- iseiti
+    
     """)
-    choice = int(input("Pasirinkite funkciją: "))
+    choice = int(input("pasirinkite funkcija: "))
 
     if choice == 1:
-        name = input("Produkto pavadinimas: ")
-        unit = input("Produkto matavimo vienetai (kg/l): ")
-        add_product(name, unit)
+        name = input("produkto pavadinimas: ")
+        
+        
+        
+        
+
+        add_product(name)
+
+        
 
     elif choice == 2:
+        
         print(saldytuvas)
         remove_product()
         print(saldytuvas)
@@ -67,11 +62,9 @@ while True:
     elif choice == 3:
         view_product()
 
-    elif choice == 4:
-        total_mass()
-
     elif choice == 0:
         print("Viso gero")
         break
     else:
-        print('Neteisingas pasirinkimas')
+        print('invalid Choice')
+        
