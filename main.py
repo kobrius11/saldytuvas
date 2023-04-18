7b8ae8be9a67a45f1ae0ee18d86991912b28b8dc
RenaldasZ <spikeris2@gmail.com>
2023-04-18 15:55:59 +0300

    last clean update

--- main.py
+++ main.py
@@ -1,74 +1,64 @@
-
 saldytuvas = {}
 
-
-
 def remove_product():
-    name = input('iveskite produkta: ')
+    name = input('Įveskite produkto pavadinimą: ')
 
     if name in saldytuvas.keys():
         if saldytuvas[name][0] > 1:
             saldytuvas[name][0] -= 1
         else:
             saldytuvas.pop(name)
-        return 
-
 
 def add_product(product_name):
     quantity = float(input("Įveskite kiekį: "))
-    unit = input("unit: ")
+    unit = input("vienetai: ")
+
     if unit == "kg":
-        quantity *= 1  # convert kg to g
+        quantity *= 1000  # convert kg to g
+
     if product_name in saldytuvas: 
         saldytuvas[product_name] += [quantity, unit]
     else:
         saldytuvas[product_name] = [quantity, unit]
+    
     print("Produktas sėkmingai pridėtas į šaldytuvą!\n")
     print(saldytuvas)
 
 def view_product():
     for key, value in saldytuvas.items():
-        print( key, value)
-
+        print(key, value)
 
 def total_mass():
     total_kg = 0
     total_l = 0
     total_vienetas = 0
+
     for value in saldytuvas.values():
         if value[1] == 'kg':
-            total_kg += value[0]
+            total_kg += value[0] / 1000  # convert g to kg
         elif value[1] == 'l':
             total_l += value[0]
         else:
             total_vienetas += value[0]
-    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l, {} vienetai".format(total_kg/1, total_l, total_vienetas))
- 
+
+    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l, {} vienetai".format(total_kg, total_l, total_vienetas))
 
 while True:
     print("""
-    1- prideti produkta
-    2- isimti produkta
-    3- apziureti produktus saldytuve
+    1 - Pridėti produktą
+    2 - Išimti produktą
+    3 - Apžiūrėti produktus šaldytuve
 
-    0- iseiti
-    
+    0 - Išeiti
     """)
-    choice = int(input("pasirinkite funkcija: "))
+
+    choice = int(input("Pasirinkite funkciją: "))
 
     if choice == 1:
-        name = input("produkto pavadinimas: ")
-        
-        
-        
-        
-
+        name = input("Produkto pavadinimas: ")
         add_product(name)
 
-        
-
     elif choice == 2:
-        
         print(saldytuvas)
         remove_product()
         print(saldytuvas)
@@ -80,6 +70,6 @@ while True:
     elif choice == 0:
         print("Viso gero")
         break
+
     else:
-        print('invalid Choice')
-        
+        print('Neteisingas pasirinkimas.')
