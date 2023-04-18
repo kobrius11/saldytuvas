6b7aef646bb73817e151b87ba4d9c53aba593b5e
RenaldasZ <130555392+RenaldasZ@users.noreply.github.com>
2023-04-18 15:08:13 +0300

    update 15:05 (#9)
    * update
    
    * update su total mass
    
    * update paskutinis
    
    ---------
    
    Co-authored-by: Zygimantas B <kobrius11@gmail.com>

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-saldytuvas = {"morkos" : 2, "burokai" : 2, "bananai" : 5, "bulves:" : 7, "pienas" : 1, "apelsinai" : 2}
+
+saldytuvas = {}
 
 
 
 def remove_product():
     name = input('iveskite produkta: ')
     if name in saldytuvas.keys():
-        ###
-        if saldytuvas[name].get("quantity") > 1:
-            ###
-            istrinti = saldytuvas.get(name).get("quantity")
+        if saldytuvas[name] > 1:
+            istrinti = saldytuvas.get(name)
             saldytuvas[name] = istrinti - 1
         else:
             saldytuvas.pop(name)
         return 
 
-def add_product(product_name, quantity=1):
-    ###
 
-    product_type = bool(input("ar produktas skystas ?: "))
+def add_product(product_name, unit):
     quantity = float(input("Įveskite kiekį: "))
     if product_name in saldytuvas: 
-        saldytuvas[product_name] += {"quantity": quantity, "Type": product_type}
+        saldytuvas[product_name] += quantity
     else:
-        saldytuvas[product_name] = {"quantity": quantity, "Type": product_type}
+        saldytuvas[product_name] = quantity
     print("Produktas sėkmingai pridėtas į šaldytuvą!\n")
     print(saldytuvas)
 
 def view_product():
     for key, value in saldytuvas.items():
         print( key, value)
+
+
+def total_mass():
+    total_kg = 0
+    total_l = 0
+    for key, value in saldytuvas.items():
+        if key in ["morkos", "burokai", "bulves"]:
+            total_kg += value
+        elif key in ["pienas"]:
+            total_l += value
+        elif key in ["bananai", "apelsinai"]:
+            total_kg += value * 0.5  # assuming an average weight of 0.5 kg per fruit
+    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l".format(total_kg/1, total_l))
 
 
 while True:
