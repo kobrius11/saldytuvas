d606c5a83b7eb38dc9efc1bfe0b97b57a7a1dd6c
RenaldasZ <spikeris2@gmail.com>
2023-04-18 11:28:42 +0300

    update

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def remove_product():
     pass
 
 def add_product():
-    pass
+    product_name = input("Įveskite produkto pavadinimą: ")
+    quantity = float(input("Įveskite kiekį: "))
+    saldytuvas[product_name] = quantity
+    print("Produktas sėkmingai pridėtas į šaldytuvą!\n")
 
 def view_product():
     pass
