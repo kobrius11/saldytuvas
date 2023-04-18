196bc73061413d8e5d4abade81d32d7fc4853721
Kobrius11 <kobrius11@gmail.com>
2023-04-18 11:31:46 +0300

    meniu

--- main.py
+++ main.py
@@ -4,8 +4,48 @@ saldytuvas = {}
 def remove_product():
     pass
 
-def add_product():
+def add_product(product_name, product_type, quantity=1):
     pass
 
 def view_product():
     pass
+
+
+while True:
+    print("""
+    1- prideti produkta
+    2- isimti produkta
+    3- apziureti produktus saldytuve
+
+    0- iseiti
+    
+    """)
+    choice = int(input("pasirinkite funkcija: "))
+
+    if choice == 1:
+        name = input("produkto pavadinimas: ")
+        tipas = input("ar produktas yra skaiciuojamas litrais ?: ")
+        kiekis = int(input("kiekis idedamas i saldytuva: "))
+        
+        
+        if tipas.lower() == "taip":
+            tipas = True
+        else:
+            tipas = False
+
+        add_product(name, tipas)
+
+        
+
+    elif choice == 2:
+        pass
+
+    elif choice == 3:
+        pass
+
+    elif choice == 0:
+        print("Viso gero")
+        break
+    else:
+        print('invalid Choice')
+        
